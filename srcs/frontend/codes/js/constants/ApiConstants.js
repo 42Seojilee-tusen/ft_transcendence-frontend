3e67d9cc34fd2a29a88b4cf34902c577a31bc3c2
gmumdal <shinhs6384@naver.com>
2025-03-08 16:40:56 +0900

    build: 자동으로 oauth_uri 넣도록 변경

--- srcs/frontend/codes/js/constants/ApiConstants.js
+++ srcs/frontend/codes/js/constants/ApiConstants.js
@@ -1,3 +1,3 @@
 export const WSS_PROTOCOL = "wss://";
 export const HOST = window.location.hostname;
-export const OAUTH_URI = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9cc4234df8bf6cc06966f0a4de040dfdace620f2d91bdcf0a375dc588b60f48d&redirect_uri=https%3A%2F%2F${HOST}%2Fcallback&response_type=code`;
+export const OAUTH_URI = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9cc4234df8bf6cc06966f0a4de040dfdace620f2d91bdcf0a375dc588b60f48d&redirect_uri=https%3A%2F%2F10.12.2.5%2Fcallback&response_type=code';
